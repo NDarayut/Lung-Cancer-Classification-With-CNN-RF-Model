ff49167da97f0b85d926d94b7b95825ed3396765
Yut <121371082+Skiiidoodle@users.noreply.github.com>
2024-08-21 11:37:06 +0700

    Update test_model.py

--- model_code/test_model.py
+++ model_code/test_model.py
@@ -7,19 +7,29 @@ from sklearn.metrics import classification_report
 import matplotlib.pyplot as plt
 import random
 
-
+# Load the model from the directory
 feature_extractor = load_model("final_model\custom_cnn_feature_extractor_finale.h5")
 classification_model = joblib.load("final_model\RF_model_finale.pkl")
 
 def accuracy(Y_pred, Y):
+    """
+    This functions sums up all the correct prediction and divide it by total instances 
+    to achieve total accuracy.
+    """
     return np.sum( Y_pred == Y) / Y.size
 
 def test_accuracy(X, Y):
+    """
+    This function uses the model to make prediction and return the accuracy as well as the predicted outcome.
+    """
     features = feature_extractor.predict(X)
     Y_pred = classification_model.predict(features)
     return accuracy(Y_pred, Y), Y_pred
 
 def predict(X):
+    """
+    This function predict a single instance and return the prediction as well as its confidence level.
+    """
     features = feature_extractor.predict(X)
     Y_pred = classification_model.predict(features)
     probability = np.max(classification_model.predict_proba(features), axis=1)
@@ -27,7 +37,9 @@ def predict(X):
     return Y_pred, probability
 
 def test_prediction(index, X, Y):
-    
+    """
+    This function is used to plot an image and its true label in conjunction with the prediction and its confidence level.
+    """
     current = X[index]
     label = Y[index]
     current_image = np.expand_dims(current, axis=0)
@@ -46,11 +58,14 @@ def test_prediction(index, X, Y):
     plt.axis("off")
     plt.show()
 
+# Accuracy accross the testing set as well as its predicted outcome.
 acc, Y_pred = test_accuracy(X, Y)
 
+# The predicted outcome is used to generate a detailed reports.
 print(f"Accuracy: {acc * 100}%")
 print(classification_report(Y, Y_pred))
 
+# randomly select 5 images and make a prediction on it.
 for i in range(5): 
     index = random.randint(1, 500)
     test_prediction(index, X, Y)
