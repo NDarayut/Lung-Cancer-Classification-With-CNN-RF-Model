e77b59c47f1fe4779dbe5e4802780c2c6ece22fa
Yut <121371082+Skiiidoodle@users.noreply.github.com>
2024-08-24 11:46:02 +0700

    Update data_preprocessing.py

--- model_code/data_preprocessing.py
+++ model_code/data_preprocessing.py
@@ -26,24 +26,27 @@ for class_name in classes:
         # Get the full path to the image file
         image_path = os.path.join(class_dir, filename)
 
-        # Load the image using OpenCV or PIL (adjust as needed)
-        image = cv2.imread(image_path)  # Replace with your preferred image loading function
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        image = cv2.resize(image, (image_size, image_size))
+        # Load the image using OpenCV 
+        image = cv2.imread(image_path)  # load the image
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # convert it RGB for 3 color channel
+        image = cv2.resize(image, (image_size, image_size)) # resize to 224x224
+        
         # Append the image and its corresponding label to the lists
         images.append(image)
         labels.append(class_name)
 
+# Encode all the label to numeric values
 label_dict = {"Bengin case": 0, "Malignant case": 1, "Normal case": 2}
 encoded_labels = [label_dict[label] for label in labels]
 
-# Generate a permutation of indices
+# Generate a permutation of indices to perform random shuffling
 permutation = np.random.permutation(len(encoded_labels))
 
 X = np.array(images)
-X = X / 255.
+X = X / 255. # Normalize the pixel scale to 0-1 (0 = black, 1 = white)
 Y = np.array(encoded_labels)
 
+# shuffle images and labels
 X = X[permutation]
 Y = Y[permutation]
 
